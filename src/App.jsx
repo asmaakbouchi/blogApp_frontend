2c932faa56f1799a44cd5b347e10be8b92b60985
asmaakbouchi <asmaakbouchi@gmail.com>
2024-03-30 15:29:27 +0100

    install react and tailwind css with the first templlate app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './component/header'
+
+
+export default function App() {
+  return (
+   <div>
+    <Header/>
+   </div>
+  )
+}
+
+
---
+++ src/component/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <div  className="bg-gray-800 text-white p-4 text-center">
+            Hello word
+        </div>
+    );
+}
+
+export default Header;
