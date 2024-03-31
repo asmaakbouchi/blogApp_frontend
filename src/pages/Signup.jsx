77c068555e8cbde04795767672436d5e3d8916e9
asmaakbouchi <asmaakbouchi@gmail.com>
2024-04-01 00:27:42 +0200

    add signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,57 +1,78 @@
-import { Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { useState } from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useNavigate} from 'react-router-dom';
+import { Alert, Button, Label, TextInput } from 'flowbite-react';
 
-export default function signup() {
-  const [formData,setFormData]=useState({});
+export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate=useNavigate();
 
-  const handleChange=(e)=>{
-    setFormData({...formData,[e.target.id]:e.target.value})
-  }
-  const handleSubmit= async (e)=>{
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    try{
-      const res =await fetch('/api/users/signup',{
-        method:'POST',
-        headers:{'Content-Type':'application/json'},
-        body: JSON.stringify(formData)
-      })
-      const data=await res.json();
+    if (!formData.name || !formData.email || !formData.password) {
+      setErrorMessage('Please fill out all fields.');
+      return;
     }
-    catch(err){
-      console.log(err)
-    }
-  }
 
-  console.log(formData);
+    try {
+      const res = await fetch('http://localhost:3000/users/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      
+      //Handling errors
+      if(data.success===false){
+        return setErrorMessage(data.message)
+      }
+
+      //if everything is well navigate to login 
+      if(res.ok){ navigate('/login')}
+
+
+    } catch (err) {
+      setErrorMessage(err.message)
+    }
+  };
+
   return (
     <div className='min-h-screen mt-20'>
-      <div className="p-4 max-w-2xl mx-auto">
-       <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <div>
-          <Label>Username</Label>
-          <TextInput type='text' placeholder='Username' id='name' onChange={handleChange}/>
-        </div>
-        <div>
-          <Label>Email</Label>
-          <TextInput type='email' placeholder='Email' id='email' onChange={handleChange}/>
-        </div>
-        <div>
-          <Label>Password</Label>
-          <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
-        </div>
-       <Button gradientDuoTone='purpleToPink' type='submit'>
-          Signup
-       </Button>
-       <div className="flex gap-2 text-sm mt-3">
-        <span>Have an account </span>
-          <Link to='/login' className='text-blue-500'>Login</Link>
-       </div>
-      
+      <div className='p-4 max-w-2xl mx-auto'>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+          <div>
+            <Label>Username</Label>
+            <TextInput type='text' placeholder='Username' id='name' onChange={handleChange} />
+          </div>
+          <div>
+            <Label>Email</Label>
+            <TextInput type='email' placeholder='Email' id='email' onChange={handleChange} />
+          </div>
+          <div>
+            <Label>Password</Label>
+            <TextInput type='password' placeholder='Password' id='password' onChange={handleChange} />
+          </div>
+          <Button gradientDuoTone='purpleToPink' type='submit'>
+            Signup
+          </Button>
+          <div className='flex gap-2 text-sm mt-3'>
+            <span>Have an account </span>
+            <Link to='/login' className='text-blue-500'>
+              Login
+            </Link>
+          </div>
 
-       </form>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+        </form>
       </div>
     </div>
-  )
+  );
 }
